4a183b8a472d40ec30554b938049ef55e0c02a17
Franco-Salgado <franco.salgados@usm.cl>
2022-12-06 13:42:30 -0300

    Alm. persistente coherente con la BLE conn.

--- App.js
+++ App.js
@@ -1,111 +1,61 @@
-import React, { useState } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, TextInput, View, Alert, TouchableOpacity } from 'react-native';
+import React, { useContext, createContext, useState, useEffect } from 'react';
+import { View, Button, Text, TextInput, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  const [temperatura, setTemperatura] = useState(null);
-  const [humedad, setHumedad] = useState(null);
-  const [dic, setDic] = useState({ sensor1: {t: [10, 12], h: [20, 8]}, sensor2: {s: [4, 6], a: [99, 98]} });
+const App = () => {
+  const key_sensor1 = '@Sensor1'
+  const key_sensor2 = '@Sensor2'
+  const [value, setValue] = useState("");
 
-  // AsyncStorage con objetos
-  const buttonSave = () => {
-    dic.sensor1.t.push(temperatura);
-    dic.sensor1.h.push(humedad);
-    AsyncStorage.setItem('objeto', JSON.stringify(dic))
-    .then(json => alert('Objeto guardado'))
-    .catch(error => alert('No hay objeto que guardar'));
+  const [temp, setTemp] = useState(23);
+  const [hum, setHum] = useState(5);
+  const [pre, setPre] = useState(1601);
+  const [temp2, setTemp2] = useState(18);
+  const [uv, setUv] = useState(2);
+  var sensor1 = {}
+  var sensor2 = {}
+
+  const setItem = async (key, dato) => {
+    try {
+      await AsyncStorage.setItem(key, dato);
+      console.log(dato)
+      alert('Dato almacenado');
+    } catch(e) {
+      alert('Error al almacenar');
+    }
   }
-  const buttonShow = () => {
-    AsyncStorage.getItem('objeto')
-    .then(req => JSON.parse(req))
-    .then(json => alert('Objeto mostrado'))
-    .catch(error => alert('No hay objeto que mostrar'));
+
+  const getItem = async (key) => {
+    try {
+      setValue(await AsyncStorage.getItem(key));
+      console.log('   VALUE: ', await AsyncStorage.getItem(key));
+      alert('Dato recuperado');
+    } catch(e) {
+      alert('Error al recuperar');
+    }
   }
+
+  const readSensor1 = () => {
+    // Toda la conexión con el sensor 1
+    // ...
+    // Lectura de los datos
+    sensor1['t'] = temp;
+    sensor1['h'] = hum;
+    sensor1['p'] = pre;
+  }
+
+  useEffect(() => {
+    getItem(key_sensor1);
+  }, [])
 
   return (
-    <View style={styles.containerV}>
-      <Text>Sensor 1</Text>
-      <View style={styles.containerH}>
-        <TextInput
-          style={styles.input}
-          onChangeText={setTemperatura}
-          value={temperatura}
-          placeholder="T"
-          keyboardType="numeric"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setHumedad}
-          value={humedad}
-          placeholder="H"
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={styles.containerH}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={buttonSave}
-        >
-          <Text>Save</Text> 
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={buttonShow}
-        >
-          <Text>Show</Text> 
-        </TouchableOpacity>
-      </View>
-      <Text style={{margin:10}}>
-        {JSON.stringify(dic)}
-      </Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Tomar muestra" onPress={() => readSensor1()} />
+      <Button title="Almacenar muestra" onPress={() => setItem(key_sensor1, JSON.stringify(sensor1))} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  containerV: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerH: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
-    margin: 5
-  }
-});
-
-
-
-
-// ======================================================================================================
-// AsyncStorage con strings
-// const botonSave = () => {
-//   if (temperatura) {
-//     AsyncStorage.setItem('string', temperatura);
-//     setTemperatura('');
-//     alert('String guardado');
-//   } else {
-//     alert('No hay string que guardar');
-//   }
-// }
-// const botonShow = () => {
-//   AsyncStorage.getItem('string').then((value) => setValue(value));
-// }
+export default () => (
+  <App />
+);
